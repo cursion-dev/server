5a17d6dc6fff11e27c9f5c82c9bb57ca1de2627d
Landon Roddenberry <siterack.io@gmail.com>
2021-11-23 09:45:14 -0600

    fixing cors header issue

--- app/scanerr/settings.py
+++ app/scanerr/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'api',
     'rest_framework',
     'rest_framework.authtoken',
