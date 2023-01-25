0fe9e1aec6c8a5d7fbaf274c623b3091c638673a
landon <l.rodden52@gmail.com>
2023-01-24 20:49:54 -0600

    testing

--- app/api/utils/lighthouse.py
+++ app/api/utils/lighthouse.py
@@ -40,72 +40,73 @@ class Lighthouse():
         # try:
         stdout_value = self.init_audit() 
         stdout_string = str(stdout_value)
-        # print(f'LH string output => {stdout_string}')
+        delm = '{\\n  "lighthouseVersion"'
+        stdout_string = delm + stdout_string.split(delm)[1]
 
     
         if len(stdout_string) != 0:
-            # if 'Runtime error encountered' in stdout_string:
-            #     error = {'error': 'lighthouse ran into a problem',}
-            #     return error
+            if 'Runtime error encountered' in stdout_string:
+                error = {'error': 'lighthouse ran into a problem',}
+                return error
 
-            # stdout_json = json.loads(stdout_value)
+            stdout_json = json.loads(stdout_value)
 
-            # # initial audits object
-            # audits = {
-            #     "seo": [],
-            #     "accessibility": [],
-            #     "performance": [],
-            #     "best-practices": [],
-            #     "lighthouse-plugin-crux": [],
-            #     "pwa": []
-            # }
+            # initial audits object
+            audits = {
+                "seo": [],
+                "accessibility": [],
+                "performance": [],
+                "best-practices": [],
+                "lighthouse-plugin-crux": [],
+                "pwa": []
+            }
 
-            # # iterating through categories to get relevant lh_audits and store them in their respective `audits = {}` obj
-            # for cat in audits:
-            #     cat_audits = stdout_json["categories"].get(cat).get("auditRefs")
-            #     if cat_audits is not None:
-            #         for a in cat_audits:
-            #             if int(a["weight"]) > 0:
-            #                 audit = stdout_json["audits"][a["id"]]
-            #                 audits[cat].append(audit)
-            # # changing audits names
-            # audits['best_practices'] = audits.pop('best-practices')
-            # audits['crux'] = audits.pop('lighthouse-plugin-crux')
+            # iterating through categories to get relevant lh_audits and store them in their respective `audits = {}` obj
+            for cat in audits:
+                cat_audits = stdout_json["categories"].get(cat).get("auditRefs")
+                if cat_audits is not None:
+                    for a in cat_audits:
+                        if int(a["weight"]) > 0:
+                            audit = stdout_json["audits"][a["id"]]
+                            audits[cat].append(audit)
+            # changing audits names
+            audits['best_practices'] = audits.pop('best-practices')
+            audits['crux'] = audits.pop('lighthouse-plugin-crux')
             
-            # # get scores from each category
-            # seo_score = round(stdout_json["categories"]["seo"]["score"] * 100)
-            # accessibility_score = round(stdout_json["categories"]["accessibility"]["score"] * 100)
-            # performance_score = round(stdout_json["categories"]["performance"]["score"] * 100)
-            # best_practices_score = round(stdout_json["categories"]["best-practices"]["score"] * 100)
-            # pwa_score = round(stdout_json["categories"]["pwa"]["score"] * 100)
-            # crux_score = round(stdout_json["categories"]["lighthouse-plugin-crux"]["score"] * 100)
+            # get scores from each category
+            seo_score = round(stdout_json["categories"]["seo"]["score"] * 100)
+            accessibility_score = round(stdout_json["categories"]["accessibility"]["score"] * 100)
+            performance_score = round(stdout_json["categories"]["performance"]["score"] * 100)
+            best_practices_score = round(stdout_json["categories"]["best-practices"]["score"] * 100)
+            pwa_score = round(stdout_json["categories"]["pwa"]["score"] * 100)
+            crux_score = round(stdout_json["categories"]["lighthouse-plugin-crux"]["score"] * 100)
 
-            # if crux_score == 0 :
-            #     crux_score = None
-            #     average_score = round((
-            #             seo_score + accessibility_score + performance_score 
-            #             + best_practices_score + pwa_score
-            #         )/ 5)
-            # else:
-            #     average_score = round((
-            #             seo_score + accessibility_score + performance_score 
-            #             + best_practices_score + pwa_score + crux_score
-            #         )/ 6)
+            if crux_score == 0 :
+                crux_score = None
+                average_score = round((
+                        seo_score + accessibility_score + performance_score 
+                        + best_practices_score + pwa_score
+                    )/ 5)
+            else:
+                average_score = round((
+                        seo_score + accessibility_score + performance_score 
+                        + best_practices_score + pwa_score + crux_score
+                    )/ 6)
 
-            # scores = {
-            #     "seo": seo_score,
-            #     "accessibility": accessibility_score,
-            #     "performance": performance_score,
-            #     "best_practices": best_practices_score,
-            #     "pwa": pwa_score,
-            #     "crux": crux_score,
-            #     "average": average_score
-            # }
+            scores = {
+                "seo": seo_score,
+                "accessibility": accessibility_score,
+                "performance": performance_score,
+                "best_practices": best_practices_score,
+                "pwa": pwa_score,
+                "crux": crux_score,
+                "average": average_score
+            }
 
 
             data = {
-                "scores": 'scores', 
-                "audits": stdout_string,
+                "scores": scores, 
+                "audits": audits,
                 "failed": False
             }
 
