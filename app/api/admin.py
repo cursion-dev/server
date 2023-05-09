9484debf4e9015df08ccf88bf54e782140aceb47
landon <l.rodden52@gmail.com>
2023-05-08 20:44:18 -0500

    testing

--- app/api/admin.py
+++ app/api/admin.py
@@ -33,7 +33,7 @@ class AccountAdmin(admin.ModelAdmin):
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('user__username', 'account__name', 'time_created', 'type', 'status')
+    list_display = ('user', 'account', 'time_created', 'type', 'status')
     search_fields = ('user__username', 'account__name')
     # list_display = ('__str__', 'time_created')
     # search_fields = ('__str__',)
