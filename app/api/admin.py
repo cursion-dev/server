633afe2534996fed7a5e6f5f4b3ded1606ce2e8c
landon <l.rodden52@gmail.com>
2023-05-08 20:34:38 -0500

    cleaning things up

--- app/api/admin.py
+++ app/api/admin.py
@@ -8,10 +8,12 @@ class SiteAdmin(admin.ModelAdmin):
     list_display = ('site_url', 'user', 'time_created')
     search_fields = ('site_url',)
 
+
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
     list_display = ('id', 'site', 'time_created', 'time_completed', 'type')
     search_fields = ('site',)
+
 
 @admin.register(Scan)
 class ScanAdmin(admin.ModelAdmin):
@@ -23,45 +25,53 @@ class ScanAdmin(admin.ModelAdmin):
         queryset.update(time_completed=datetime.now())
 
 
-
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'time_created', 'type')
     search_fields = ('__str__',)
+
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('user', 'account__name', 'time_created', 'type', 'status')
     search_fields = ('user__username', 'account__name')
 
+
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'brand', 'last_four')
     search_fields = ('last_four',)
 
+
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'time_created', 'user')
+
 
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'time_created', 'status', 'user')
 
+
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'time_created', 'status', 'user')
+
 
 @admin.register(Automation)
 class AutomationAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'time_created', 'schedule', 'user')
 
+
 @admin.register(Process)
 class ProcessAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'time_created',  'time_completed', 'progress', 'successful')
 
+
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'user', 'time_created',)
+
 
 @admin.register(Testcase)
 class TestcaseAdmin(admin.ModelAdmin):
