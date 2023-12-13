efbb05705e26ca606542d423d386f637fe322e75
Landon <l.rodden52@gmail.com>
2023-12-13 10:14:36 -0500

    testing debug options

--- app/api/utils/yellowlab.py
+++ app/api/utils/yellowlab.py
@@ -18,7 +18,7 @@ class Yellowlab():
     
     def init_audit(self):
         proc = subprocess.Popen([
-                'DEBUG=ylt:* node bin/cli.js',  # yellowlabtools
+                'yellowlabtools DEBUG=ylt:* node bin/cli.js',  # yellowlabtools
                 self.page.page_url,
                 f'--device={self.configs["device"]}'
                 ], 
