6c45d028af59abae330c35af3721332ce5bfd021
Landon <l.rodden52@gmail.com>
2024-03-05 10:56:54 -0700

    added dynamic debuging

--- app/scanerr/settings.py
+++ app/scanerr/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ['*']
 CLIENT_URL_ROOT = os.environ.get('CLIENT_URL_ROOT')
