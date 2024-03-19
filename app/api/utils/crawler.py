09e92842d87857ff7118a32d822344afa3fa5126
Landon <l.rodden52@gmail.com>
2024-03-19 13:12:24 -0600

    added driver wait

--- app/api/utils/crawler.py
+++ app/api/utils/crawler.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from .driver_s import driver_init, quit_driver
+from .driver_s import *
 
 
 
@@ -48,6 +48,14 @@ class Crawler():
         
         def add_urls(start_url):
             self.driver.get(start_url)
+
+            # adding wait for crawler
+            driver_wait(
+                driver=self.driver,
+                max_wait_time=20, 
+                interval=2
+            )
+
             soup = BeautifulSoup(self.driver.page_source, 'html.parser')
             print(self.driver.page_source)
             print(soup)
