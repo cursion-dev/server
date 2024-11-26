ae5290bea01b80fb7eb7269fdf7b090c80d8a8c4
Landon <landon@greylabs.io>
2024-11-26 09:56:02 -0700

    new configs

--- app/cursion/settings.py
+++ app/cursion/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Static file service without nginx
