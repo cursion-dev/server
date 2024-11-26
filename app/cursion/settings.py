eaba6b62de2963ae354503f77b121d1ef1d2370d
Landon <landon@greylabs.io>
2024-11-26 11:23:01 -0700

    new configs

--- app/cursion/settings.py
+++ app/cursion/settings.py
@@ -154,13 +154,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Static file service without nginx
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
 # Used to authenticate with S3 using 'django-stores' pypi package and 'boto3'
