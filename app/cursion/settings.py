786e4589b11e37744e671f115b643147281b1ef3
Landon <landon@greylabs.io>
2025-05-19 15:36:53 -0600

    update: disabled `server_side_cursors` for DB

--- app/cursion/settings.py
+++ app/cursion/settings.py
@@ -106,7 +106,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('DB_PORT')
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {'server_side_cursors': False}
     }
 }
 
