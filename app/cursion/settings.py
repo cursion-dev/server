9f7716b083366d1833e7ec82b1824b96bcd9de11
Landon <landon@greylabs.io>
2025-05-19 16:08:21 -0600

    fix: removed OPTIONS from DB

--- app/cursion/settings.py
+++ app/cursion/settings.py
@@ -106,8 +106,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('DB_PORT'),
-        'OPTIONS': {'server_side_cursors': False}
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
